47ce68fe6dabb7f444686b3789f76cddec4cf67b
Carmenpd29 <cpulidod01@educarex.com>
2025-05-27 13:53:30 +0200

    20252705-s4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import React, { useState } from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <GlobalStyles/>
-        <span>Hola Mundo</span>
+      <GlobalStyles />
+      <span>Hola Mundo</span>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
