2c1a675bfef53267d5765929cbb25a95e58d0bbd
Carmenpd29 <cpulidod01@educarex.com>
2025-06-02 10:50:13 +0200

    20250206-2

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Reemplaza estos valores por los de tu proyecto Supabase
 const supabaseUrl = 'https://ctvhujhlgyqwekgjuajs.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0dmh1amhsZ3lxd2VrZ2p1YWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNDkwNzQsImV4cCI6MjA2MzgyNTA3NH0.u4DtFHgOH4g9Ac4GjfGxZPAvW2rlFojSbZDmGKtwaW4';
 
