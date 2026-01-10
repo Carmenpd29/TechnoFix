637efdfb3cfda159ecfc319180a8135c6714c726
Carmenpd29 <cpulidod01@educarex.com>
2026-01-10 12:33:03 +0100

    Changes

--- src/utils/supabaseUtils.js
+++ src/utils/supabaseUtils.js
@@ -1,11 +1,20 @@
 // Helper para asegurar timeout en llamadas a promesas (ej. supabase)
 export const withTimeout = (promise, ms = 10000) => {
   let timeoutId;
-  const timeoutPromise = new Promise((_, reject) => {
+  const p = Promise.resolve(promise);
+
+  return new Promise((resolve, reject) => {
     timeoutId = setTimeout(() => reject(new Error('timeout')), ms);
+
+    p.then(
+      (res) => {
+        clearTimeout(timeoutId);
+        resolve(res);
+      },
+      (err) => {
+        clearTimeout(timeoutId);
+        reject(err);
+      }
+    );
   });
-  return Promise.race([
-    promise.finally(() => clearTimeout(timeoutId)),
-    timeoutPromise,
-  ]);
 };
